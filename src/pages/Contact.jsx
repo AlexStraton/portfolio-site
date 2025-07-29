7aa9b9ad389937ea12e3a2e17a5f08d3c64bf10b
AlexStraton <alexandra.straton@curaleafint.com>
2025-07-29 18:32:48 -0400

    Recofigure Vite config

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,8 @@
+import Footer from "../components/Footer"
+
 export default function Contact() {
-  return <h1 className="mt-24">Contact</h1>;
+  return ( <>
+  <h1 className="mt-24">Contact</h1>
+
+  </>)
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,10 @@
+
+
 export default function Projects() {
-  return <h1 className="mt-24">My Projects</h1>;
+
+
+  return <>
+  <h1 className="mt-24">My Projects</h1>
+
+</>
 }
